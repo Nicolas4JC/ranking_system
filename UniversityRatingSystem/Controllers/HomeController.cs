5cd79caa3f18f24596b997c552d251403898ed09
Bogdan Urazakaev <bogdan.urazakaew@gmail.com>
2014-11-13 11:52:34 +0300

    Initial commit

---
+++ UniversityRatingSystem/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UniversityRatingSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(HomeController));
+
+        public ActionResult Index()
+        {
+            logger.Info("Index accessed");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            logger.Info("About page accessed");
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            logger.Info("Contact page accessed");
+            return View();
+        }
+    }
+}
---
+++ UniversityRatingSystem/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UniversityRatingSystem.Startup))]
+namespace UniversityRatingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            log4net.Config.XmlConfigurator.Configure();
+        }
+    }
+}
